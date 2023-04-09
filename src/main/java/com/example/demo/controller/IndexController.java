3ef421bfb889e81d89483be5f392c73004950774
asd000qaq <1282423878@qq.com>
2023-04-09 19:48:18 +0800

    asdadada

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/index")
+    public String sayHello(){
+        return "index";
+    }
+}
